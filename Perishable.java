46c1d1563ad85e876933962e9d6c777ed06f8a9e
Darya <darya.rempel@bk.ru>
2023-02-17 21:50:25 +0600

    Perishable

---
+++ Perishable.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+public class Perishable extends Product{   //extends -наследование у продукт класса
+
+    private Date expireDate = new Date();
+
+    public Perishable(String name, double cost, Date expireDate){
+        super(name,cost);
+        this.expireDate = expireDate;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder localString = new StringBuilder(super.toString());
+        localString.append(String.format("годен до %s",expireDate.toString()));
+        return localString.toString();
+    }
+}
